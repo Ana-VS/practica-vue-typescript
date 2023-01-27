190cd6098b23883bd6b8d95e14a5be28c813ad79
Ana Virseda <ana.vs21685@gmail.com>
2023-01-27 19:16:46 +0100

    Creación componentes NavBar y CustomButton

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,29 @@
+<template>
+    <button>
+        <slot></slot>
+        <slot name="icon"></slot>
+    </button>
+</template>
+
+<style scoped>
+button {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-family: 'Roboto' sans-serif;
+    font-weight: bold;
+    color: #35495e;
+    box-sizing: border-box;
+    padding: 10px;
+    border-radius: 15px;
+    background-color: #41b883;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+}
+button:hover, button:focus, .button-active {
+    box-shadow: 0 0 20px rgba(0,0,0,0.5) ;
+    -webkit-transform: scale(1.1);
+    transform: scale(1.1);
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,63 @@
+<template>
+   <nav>   
+        <div class="logo">
+            <img src="../assets/logo.png">
+            <h1>VueShop</h1>
+        </div>
+      
+        <input type="text" placeholder="¿Qué estas buscando?">
+      
+        <div class="buttons">
+            <custom-button class="logout">
+                <span>Log Out</span>
+                <template v-slot:icon><span>✖</span></template>            
+            </custom-button>   
+        </div>          
+   </nav>
+</template>  
+
+<script lang="ts">
+import  {defineComponent} from 'vue';
+
+import CustomButton from './CustomButton.vue';
+
+export default defineComponent({
+    name:'NavBar',
+    components: {
+        CustomButton,
+    }
+})
+</script>
+
+<style scoped>
+    nav {
+        width: 100%;
+        height: 66px;
+        border-bottom: 1px solid #35495e;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    input {
+        width: 100%;
+        height: 80%;
+        margin: 0 30px;
+        border: none;
+    }
+    .logo {
+        display:flex;
+        justify-content: space-between;
+    }
+    
+    .logout {
+        width: 100px;
+        height: 55px;
+        font-size: 13px;
+    }
+    img {height: 55px;}
+    h1 {
+        font-family: 'Roboto' sans-serif;
+        font-weight: bold;
+        color: #41b883;
+    }
+</style>
